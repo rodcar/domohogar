1353438dbc692610a0454072aca5368761ccdb81
rodcar <ivanrod12@hotmail.com>
2022-11-15 12:26:07 -0500

    agrega funcionalidad de calculo de subtotal

--- js/carrito.js
+++ js/carrito.js
@@ -3,6 +3,8 @@ import { ProductoDAO } from "./daos/ProductoDAO.js";
 
 let carritoDAO = new CarritoDAO();
 let productoDAO = new ProductoDAO();
+let subtotal = 0;
+let total = 0;
 
 document.addEventListener("DOMContentLoaded", function() {
     productoDAO.fetchAll().then(() => {
@@ -13,9 +15,12 @@ document.addEventListener("DOMContentLoaded", function() {
 // Carga de productos
 
 function loadListaDeProductos() {
+    subtotal = 0;
+    document.getElementById("carrito-cantidad").innerHTML = `Tienes ${carritoDAO.getCantidad()} productos en el carrito.`;
     document.getElementById("lista-productos").innerHTML = "";
     for(let item of carritoDAO.items) {
         let producto = productoDAO.localFetchById(item.id);
+        subtotal += producto.precio * item.cantidad;
         document.getElementById("lista-productos").innerHTML += `${item.getHTML(producto)}`;
         document.getElementById("lista-productos").innerHTML += `
         <button class="reducir-carrito" data-producto-id="${producto.id}">-</button>
@@ -34,8 +39,9 @@ function loadListaDeProductos() {
         aumentarButtons[i].onclick = (e) => aumentarCarrito(e);
         eliminarButtons[i].onclick = (e) => eliminarCarrito(e);
     }
-    //document.getElementById("aumentar-carrito").onclick = (e) => aumentarCarrito(e);
-    //document.getElementById("eliminar-carrito").onclick = (e) => EliminarCarrito(e);
+
+    // datos del pago
+    document.getElementById("subtotal").innerHTML = `S/.${subtotal}`;
 }
 
 // Eventos
