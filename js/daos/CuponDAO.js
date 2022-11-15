bb003975b1ec95cbbd9a705df8279f52a02aa8a0
rodcar <ivanrod12@hotmail.com>
2022-11-15 16:26:09 -0500

    agrega DAO de cupones

---
+++ js/daos/CuponDAO.js
@@ -0,0 +1,15 @@
+import { Cupon } from '../entidades/Cupon.js';
+import {JsonDAO} from './JsonDAO.js';
+
+export class CuponDAO extends JsonDAO{
+    constructor() {
+        super("data/cupones.json", Cupon);
+    }
+
+    async findByCodigo(codigo) {
+        let cupones = await this.fetchAll();
+        return cupones.filter(function(cupon) {
+            return cupon.codigo == codigo;
+        });
+    }
+}
---
+++ js/entidades/Cupon.js
@@ -0,0 +1,8 @@
+export class Cupon {
+    constructor(id, codigo, tipo, descuento){
+        this.id;
+        this.codigo = codigo;
+        this.tipo = tipo;
+        this.descuento = descuento;
+    }
+}
