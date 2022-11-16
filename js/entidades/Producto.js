f4351a55871e7a6521cd5ec6e107417285ba4731
rodcar <ivanrod12@hotmail.com>
2022-11-16 16:24:12 -0500

    agrega cards de producto

--- js/entidades/Producto.js
+++ js/entidades/Producto.js
@@ -18,12 +18,28 @@ export class Producto {
 
     getCardHTML() {
         return `
-            <img src="${this.images[0]}" width="100" heigth="100">
+        <div class="col">
+            <div class="card h-100">
+            <img src="${this.images[0]}" class="card-img-top" alt="${this.nombre}">
+            <div class="card-body">
+                <h5 class="card-title">${this.nombre}</h5>
+                <p class="card-text">${this.marca}</p>
+                <p class="card-text">S/.${this.precio}</p>
+                <div class="d-flex justify-content-center">
+                    <a href="producto.html?id=${this.id}" class="btn btn-primary">Ver detalle</a>
+                </div>                
+            </div>
+            </div>
+        </div>
+        `;
+
+        /*
+        <img src="${this.images[0]}" width="100" heigth="100">
             <p>S/.${this.precio}</p>
             <p>${this.marca}</p>
             <a href="producto.html?id=${this.id}">
                 ${this.nombre}
             </a>
-        `;
+        */
     }
 }
