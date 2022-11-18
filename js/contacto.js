6df850dd3384dbced64a91c10a597114b0dc2b0b
rodcar <ivanrod12@hotmail.com>
2022-11-17 21:54:40 -0500

    fix formulario de contacto

--- js/contacto.js
+++ js/contacto.js
@@ -1,38 +1,38 @@
 document.addEventListener("DOMContentLoaded", function () {
-    document.getElementById("formulario").addEventListener('submit', validarFormulario);
+    //document.getElementById("formulario").addEventListener('submit', validarFormulario);
 });
 
-function validarFormulario(evento) {
-    evento.preventDefault();
+function validarFormulario() {
+    //evento.preventDefault();
     var nombre = document.getElementById("nombre").value;
     if (nombre.length == 0) {
         alert('Por favor ingrese su nombre');
-        return;
+        return false;
     }
 
     var apellido = document.getElementById("apellido").value;
     if (apellido.length == 0) {
         alert('Por favor ingrese su apellido');
-        return;
+        return false;
     }
 
     var telefono = document.getElementById("telefono").value;
     if (telefono.length == 0) {
         alert('Por favor ingrese su teléfono');
-        return;
+        return false;
     }
 
     var correo = document.getElementById("correo").value;
     if (correo.length == 0) {
         alert('Por favor ingrese su correo');
-        return;
+        return false;
     }
 
     var comentario = document.getElementById("comentario").value;
     if (comentario.length == 0) {
         alert('Por favor ingrese un comentario');
-        return;
+        return false;
     }
 
-
+    return true;
 }
